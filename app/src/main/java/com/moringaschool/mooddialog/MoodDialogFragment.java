6dd2d2123acf5c15bbfe7d52a9d870089f3775ed
Ashisoma <okalionesmus@gmail.com>
2020-11-11 23:41:28 +0300

    add layouts

---
+++ app/src/main/java/com/moringaschool/mooddialog/MoodDialogFragment.java
@@ -0,0 +1,19 @@
+package com.moringaschool.mooddialog;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.DialogFragment;
+
+public class MoodDialogFragment extends DialogFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
+        View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
+        getDialog().setTitle("Simple Dialog");
+        return rootView;
+
+    }
+}
