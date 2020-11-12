807df0547ad31d05a0b2de3764374e94805ae0a9
Ashisoma <okalionesmus@gmail.com>
2020-11-12 16:39:39 +0300

    add fragments buttons and pop up

--- app/src/main/java/com/moringaschool/mooddialog/MainActivity.java
+++ app/src/main/java/com/moringaschool/mooddialog/MainActivity.java
@@ -1,14 +1,42 @@
 package com.moringaschool.mooddialog;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button) Button mMoodButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        ButterKnife.bind(this);
+
+        mMoodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm =  getSupportFragmentManager();
+                MoodDialogFragment moodDialogFragment = new MoodDialogFragment ();
+                moodDialogFragment.show(fm, "Sample Fragment");
+            }
+        });
+
+
+
+            }
+
 }
--- app/src/main/java/com/moringaschool/mooddialog/MoodDialogFragment.java
+++ app/src/main/java/com/moringaschool/mooddialog/MoodDialogFragment.java
@@ -1,19 +1,40 @@
 package com.moringaschool.mooddialog;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MoodDialogFragment extends DialogFragment {
+
+
+//    Button moodButton = (Button) .findViewById(R.id.cancelButton);
+//    Button submitButton = (Button) rootView.findViewById(R.id.submitButton);
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
         View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
         getDialog().setTitle("Simple Dialog");
-        return rootView;
+        Button cancelButton = (Button) rootView.findViewById(R.id.cancelButton);
+        Button submitButton = (Button) rootView.findViewById(R.id.submitButton);
 
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        return rootView;
     }
+
 }
