9c7c8c465b1c5558a54e4aaa7415ef63230c9cf3
Ashisoma <okalionesmus@gmail.com>
2020-11-12 17:03:23 +0300

    add submit button and log to the console

--- app/src/main/java/com/moringaschool/mooddialog/MoodDialogFragment.java
+++ app/src/main/java/com/moringaschool/mooddialog/MoodDialogFragment.java
@@ -2,10 +2,13 @@ package com.moringaschool.mooddialog;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
@@ -27,6 +30,19 @@ public class MoodDialogFragment extends DialogFragment {
         getDialog().setTitle("Simple Dialog");
         Button cancelButton = (Button) rootView.findViewById(R.id.cancelButton);
         Button submitButton = (Button) rootView.findViewById(R.id.submitButton);
+
+            RadioGroup surveyRadioGroup = (RadioGroup) rootView.findViewById(R.id.moodRadioGroup);// pull the button group
+        int selectedId = surveyRadioGroup.getCheckedRadioButtonId();//get selected radio button
+        final RadioButton selectedRadioButton = (RadioButton) rootView.findViewById(selectedId);// get radio button values via ID
+
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("testing", selectedRadioButton.getText().toString());
+                dismiss();
+            }
+        });
 
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
